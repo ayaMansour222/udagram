e5bb8f8142c4c212d884bcf273e80c5704a93335
ayaMansour222 <ayasayed_ibrahim@yahoo.com>
2022-05-28 12:00:24 +0200

    changes new

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb use udagram-api-dev && eb deploy
+eb use udagram-api-dev && eb deploy && eb deploy && eb setenv DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME
